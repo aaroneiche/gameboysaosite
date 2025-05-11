33da75e74ff610dfc1b214ed4ed847ab9d3ba575
Aaron Eiche <aeiche@locusrobotics.com>
2025-05-11 15:32:52 -0700

    Adds color handling to sprite builder

--- src/SpriteBuilder.tsx
+++ src/SpriteBuilder.tsx
@@ -1,35 +1,81 @@
 import { Dotting, useData, useDotting, useBrush } from "dotting";
-import type { DottingRef } from "dotting";
+import type { DottingRef, PixelModifyItem, } from "dotting";
 
-import { useEffect, useRef } from "react";
 
-const DottingExample = () => {
+import {useState, useRef } from "react";
+
+
+const CreateEmptySquareData = (
+  size: number,
+): Array<Array<PixelModifyItem>> => {
+  const data: Array<Array<PixelModifyItem>> = [];
+  for (let i = 0; i < size; i++) {
+    const row: Array<PixelModifyItem> = [];
+    for (let j = 0; j < size; j++) {
+      row.push({ rowIndex: i, columnIndex: j, color: "" });
+    }
+    data.push(row);
+  }
+  return data;
+}; 
+
+
+const DottingExample = (props:{currentColor: string}) => {
   
-    const ref = useRef<DottingRef>(null);
-    console.log(ref);
-    const { clear } = useDotting(ref);
-    const {data, dataArray} = useData(ref);
-    const { changeBrushColor } = useBrush(ref);    
+  //CreateEmptySquareData(8)
+  const ref = useRef<DottingRef>(null);
+  
+  const { clear, setData } = useDotting(ref);
+  const { data, dataArray } = useData(ref);
+  const { changeBrushColor } = useBrush(ref);
 
-    useEffect(()=>{
-        console.log(dataArray);
-    },[dataArray])
+  // useEffect(() => {
+  //   console.log(dataArray);
+  // }, [dataArray]);
 
-    useEffect(()=>{
-        console.log(data);
-    },[data])
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
 
-    return (
-        <Dotting ref={ref} width={350} height={350} isGridFixed={true} />
+
+  const initialLayer = [
+    {
+      id: "layer1",
+      data: CreateEmptySquareData(8)
+    }];
+
+  return (
+    <Dotting
+      ref={ref}
+      width={350}
+      height={350}
+      isGridFixed={true}
+      initLayers={initialLayer}
+      brushColor={props.currentColor}
+      
+    />
   );
 };
 
 export default function SpriteBuilder() {
 
+    const [currentColors] = useState<string[]>([
+      "#FFFFFF",
+      "#FF0000",
+      "#00FF00",
+      "#0000FF",
+    ]);
+    
+    // const [currentColor,setCurrentColor] = useState<typeof currentColors[string]>(currentColors[1])
+    const [currentColor,setCurrentColor] = useState(currentColors[1])
+
+    const colorSelector = currentColors.map(c=><button onClick={()=>{setCurrentColor(c)}}>{c}</button>)
+
   return (
     <>
       <div> This is the sprite builder</div>
-      <DottingExample/>
+      <DottingExample currentColor={currentColor}/>
+      {colorSelector}
     </>
   );
 }
